67a79a7bad53fbb95f064f1fd4c044975475ad89
rickboy26 <rickdh123@gmail.com>
2021-03-17 20:27:52 +0100

    Updated

--- src/main/java/com/clanevents/ClanEventsPanel.java
+++ src/main/java/com/clanevents/ClanEventsPanel.java
@@ -48,11 +48,14 @@ import net.runelite.client.util.ImageUtil;
 class ClanEventsPanel extends PluginPanel
 {
     private final JPanel ssArea = new JPanel();
+    JPanel cmButtonPanel = new JPanel();
     private final JLabel ssText = new JLabel();
     private final GoogleSheet sheet = new GoogleSheet();
     final JComboBox<ComboBoxIconEntry> dropdown = new JComboBox<>();
     private String color1;
     private String color2;
+    private String tab;
+    private int cmMan = 3;
 
     void init(ClanEventsConfig config, int index){
         // Google sheet API
@@ -70,14 +73,12 @@ class ClanEventsPanel extends PluginPanel
         dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " Home", "home"));
         icon = ImageUtil.loadImageResource(getClass(), "zuk.png");
         dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " Zuk'n Ladders", "event"));
-        icon = ImageUtil.loadImageResource(getClass(), "event.png");
-        dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " Clan Event", "event"));
         icon = ImageUtil.loadImageResource(getClass(), "hof.png");
         dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " Hall of Fame", "hof"));
         icon = ImageUtil.loadImageResource(getClass(), "botw.png");
         dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " Time Highscore", "speed"));
         icon = ImageUtil.loadImageResource(getClass(), "botw.png");
-        dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " CM Mentors", "cmmen"));
+        dropdown.addItem(new ComboBoxIconEntry(new ImageIcon(icon), " CM Meta", "cmmen"));
 
         //Set the color
         color1 = "#"+Integer.toHexString(config.col1color().getRGB()).substring(2);
@@ -88,7 +89,13 @@ class ClanEventsPanel extends PluginPanel
             if (e.getStateChange() == ItemEvent.SELECTED)
             {
                 final ComboBoxIconEntry source = (ComboBoxIconEntry) e.getItem();
-                ssText.setText(getSheetDataFormatted(sheet, source.getData().toString()));
+                tab = source.getData().toString();
+                cmButtonPanel.setVisible(false);
+
+                if (tab.equals("cmmen")) {
+                    cmButtonPanel.setVisible(true);
+                }
+                ssText.setText(getSheetDataFormatted(sheet, tab));
             }
         });
 
@@ -102,6 +109,14 @@ class ClanEventsPanel extends PluginPanel
 
         this.add(dropdown, c);
         c.gridy++;
+
+        for (int i = 3; i < 10; i++) {
+            cmButtonPanel.add(createCmButton(i), BorderLayout.WEST);
+        }
+
+        cmButtonPanel.setVisible(false);
+
+        this.add(cmButtonPanel, BorderLayout.NORTH);
 
         //Bottom Textarea
         ComboBoxIconEntry selected = (ComboBoxIconEntry) dropdown.getSelectedItem();
@@ -146,6 +161,42 @@ class ClanEventsPanel extends PluginPanel
         return label;
     }
 
+    private JButton createCmButton(int index )
+    {
+        final JButton label = new JButton(String.valueOf(index));
+        label.setPreferredSize(new Dimension(26, 26));
+        label.setFont(new Font("Arial", Font.PLAIN, 9));
+        label.setFocusable(false);
+        label.addMouseListener(new MouseAdapter()
+        {
+            @Override
+            public void mouseEntered(MouseEvent e)
+            {
+                label.setBackground(ColorScheme.DARK_GRAY_HOVER_COLOR);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e)
+            {
+                label.setBackground(ColorScheme.DARK_GRAY_COLOR);
+            }
+
+            @SneakyThrows
+            @Override
+            public void mousePressed(MouseEvent e)
+            {
+                if (e.getButton() == MouseEvent.BUTTON1)
+                {
+                    cmMan = index;
+                    ComboBoxIconEntry selected = (ComboBoxIconEntry) dropdown.getSelectedItem();
+                    ssText.setText(getSheetDataFormatted(sheet, selected.getData().toString()));
+                }
+            }
+        });
+
+        return label;
+    }
+
     private String getSheetDataFormatted(GoogleSheet sheet, String field)
     {
         try
@@ -157,35 +208,75 @@ class ClanEventsPanel extends PluginPanel
                 System.out.println("No data found.");
             } else {
                 data += "<html><table width=230>";
-                for (List row : values) {
-                    String val1 = "";
-                    String val2 = "";
+                if (tab.equals("cmmen")) {
+                    int i = 0;
+                    for (List row : values) {
+                        if (i  > 3) {
+                            break;
+                        }
+                        String val1 = "";
+                        String val2 = "";
 
-                    try
-                    {
-                        val1 = ""+row.get(0);
-                    }catch(Exception e)
-                    {
-                        val1 = "";
-                    }
-                    try
-                    {
-                        val2 = ""+row.get(1);
-                    }catch(Exception e)
-                    {
-                        val2 = "";
-                    }
+                        try
+                        {
+                            val1 = ""+row.get(0);
+                        }catch(Exception e)
+                        {
+                            val1 = "";
+                        }
+                        try
+                        {
+                            val2 = ""+row.get(1);
+                        }catch(Exception e)
+                        {
+                            val2 = "";
+                        }
 
-                    data += "<tr>";
-                    data += "<td><font color='" + color1 + "'>";
-                    data += val1;
-                    data += "</font></td>";
-                    data += "<td><font color='" + color2 + "'>";
-                    data += val2;
-                    data += "</font></td>";
-                    data += "</tr>";
+                        if (val1.equals(cmMan + " man") || i > 0) {
+                            data += "<tr>";
+                            data += "<td><font color='" + color1 + "'>";
+                            data += val1;
+                            data += "</font></td>";
+                            data += "<td><font color='" + color2 + "'>";
+                            data += val2;
+                            data += "</font></td>";
+                            data += "</tr>";
+
+                            i++;
+                        }
+                    }
+                } else {
+                    for (List row : values) {
+                        String val1 = "";
+                        String val2 = "";
+
+                        try
+                        {
+                            val1 = ""+row.get(0);
+                        }catch(Exception e)
+                        {
+                            val1 = "";
+                        }
+                        try
+                        {
+                            val2 = ""+row.get(1);
+                        }catch(Exception e)
+                        {
+                            val2 = "";
+                        }
+
+                        data += "<tr>";
+                        data += "<td><font color='" + color1 + "'>";
+                        data += val1;
+                        data += "</font></td>";
+                        data += "<td><font color='" + color2 + "'>";
+                        data += val2;
+                        data += "</font></td>";
+                        data += "</tr>";
+                    }
                 }
                 data += "</table></html>";
+
             }
             return data;
         }catch (Exception ioException)
