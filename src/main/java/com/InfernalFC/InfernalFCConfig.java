bd46b8cc1ca5f3aaea837ba35ad97f6a73d9e355
rickboy26 <rickdh123@gmail.com>
2021-04-04 03:39:27 +0200

    Changed config to load quicker.

--- src/main/java/com/InfernalFC/InfernalFCConfig.java
+++ src/main/java/com/InfernalFC/InfernalFCConfig.java
@@ -10,77 +10,90 @@ import java.awt.*;
 @ConfigGroup(InfernalFCPlugin.CONFIG_GROUP)
 public interface InfernalFCConfig extends Config
 {
-	@ConfigSection(
-			name = "Event Password",
-			description = "Password info for clan event",
-			position = 0
-	)
-	String eventPassSection = "Event Passphrase";
+		@ConfigSection(
+				name = "Event Password",
+				description = "Password info for clan event",
+				position = 0
+		)
+		String eventPassSection = "Event Passphrase";
 
-	@ConfigSection(
-			name = "Google Sheets",
-			description = "Google Sheet API Config",
-			position = 1
-	)
-	String gSheetsSection = "Google Sheets API";
+		@ConfigSection(
+				name = "Colours",
+				description = "Colour config",
+				position = 1
+		)
+		String gSheetsSection = "Google Sheets API";
 
-	@ConfigItem(
-			position = 1,
-			keyName = "eventPass",
-			name = "Event Password",
-			description = "Creates an overlay with the event password time",
-			section = eventPassSection
-	)
-	default String eventPass()
-	{
-		return "";
+		@ConfigItem(
+				position = 1,
+				keyName = "overlay",
+				name = "Display the overlay",
+				description = "Display the overlay on your game screen",
+				section = eventPassSection
+		)
+		default boolean overlay()
+		{
+			return false;
+		}
+
+		@ConfigItem(
+				position = 2,
+				keyName = "eventPass",
+				name = "Event Password",
+				description = "Creates an overlay with the event password time",
+				section = eventPassSection
+		)
+		default String eventPass()
+		{
+			return "";
+		}
+		@ConfigItem(
+				position = 3,
+				keyName = "dtm",
+				name = "Include Date & Time",
+				description = "Display the date and time",
+				section = eventPassSection
+		)
+		default boolean dtm()
+		{
+			return true;
+		}
+
+
+		@ConfigItem(
+				position = 5,
+				keyName = "passColor",
+				name = "Overlay Color",
+				description = "Configures the color of the passphrase",
+				section = eventPassSection
+		)
+		default Color passColor()
+		{
+			return Color.GREEN;
+		}
+
+
+		@ConfigItem(
+				position = 8,
+				keyName = "col1color",
+				name = "Column 1 Color",
+				description = "Configures the color of the spreadsheet column displayed",
+				section = gSheetsSection
+		)
+		default Color col1color()
+		{
+			return Color.white;
+		}
+
+		@ConfigItem(
+				position = 9,
+				keyName = "col2color",
+				name = "Column 2 Color",
+				description = "Configures the color of the spreadsheet column displayed",
+				section = gSheetsSection
+		)
+		default Color col2color()
+		{
+			return Color.GREEN;
+		}
 	}
-	@ConfigItem(
-			position = 2,
-			keyName = "dtm",
-			name = "Date & Time",
-			description = "Display the date and time",
-			section = eventPassSection
-	)
-	default boolean dtm()
-	{
-		return true;
-	}
-
-	@ConfigItem(
-			position = 4,
-			keyName = "passColor",
-			name = "Overlay Color",
-			description = "Configures the color of the passphrase",
-			section = eventPassSection
-	)
-	default Color passColor()
-	{
-		return Color.GREEN;
-	}
-
-
-	@ConfigItem(
-			position = 5,
-			keyName = "col1color",
-			name = "Column 1 Color",
-			description = "Configures the color of the spreadsheet column displayed",
-			section = gSheetsSection
-	)
-	default Color col1color()
-	{
-		return Color.white;
-	}
-
-	@ConfigItem(
-			position = 6,
-			keyName = "col2color",
-			name = "Column 2 Color",
-			description = "Configures the color of the spreadsheet column displayed",
-			section = gSheetsSection
-	)
-	default Color col2color()
-	{
-		return Color.GREEN;
-	}
-}
